7c9a6affdea43caf513112d2d68c5e77915b5e3b
Javier Martinez <javi@64robots.com>
2020-05-09 19:29:54 +0200

    add youtube

--- components/SectionSocialMedia.vue
+++ components/SectionSocialMedia.vue
@@ -3,6 +3,7 @@
     <a href="https://twitter.com/arcademula" class="nes-icon twitter is-large"></a>
     <a href="https://instagram.com/arcademula"  class="nes-icon instagram is-large"></a>
     <a href="https://facebook.com/arcademula"  class="nes-icon facebook is-large"></a>
+    <a href="https://www.youtube.com/channel/UCGsRc6AsujsqA4mIIi-VyaA"  class="nes-icon youtube is-large"></a>
   </div>
 </template>
 
