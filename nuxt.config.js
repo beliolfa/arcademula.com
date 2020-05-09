fb0d18ea4391b0e94dd5d271d8bb9338dac40145
Javier Martinez <javi@64robots.com>
2020-05-09 18:50:59 +0200

    twitter card

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@arcademula' },
+      { name: 'twitter:title', content: 'Asociación Arcade Mula. Preservación del Arcade de los 80 y 90' },
+      { name: 'twitter:description', content: 'Queremos que los niños de hoy, que no han conocido el Arcade, puedan descubrir cómo y con qué nos divertíamos los hoy mayores, y los que hoy somos mayores podamos volver a pasarlo como niños.' },
+      { name: 'twitter:image', content: 'https://pbs.twimg.com/profile_banners/1117078712799965185/1579629725/1500x500' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'https://user-images.githubusercontent.com/12644599/81475748-54e96200-920e-11ea-9b7a-3685d122de6a.png' },
